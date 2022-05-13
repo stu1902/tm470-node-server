94e20ef94159f9e735b900e35f304dcb050533de
stu1902 <stuart1902@gmail.com>
2022-05-13 15:28:25 +0100

    Add directories

--- server.js
+++ server.js
@@ -9,6 +9,18 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/user', (req, res) =>{
+  res.send('This is the user page!')
+})
+
+app.get('/location', (req, res) =>{
+  res.send('This is the location page!')
+})
+
+app.get('/route', (req, res) =>{
+  res.send('This is the route page!')
+})
+
 app.listen(port, () => {
   console.log(`Server running at http://${hostname}:${port}`)
 })
