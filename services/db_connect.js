4f2b645ef933f165e6d33c5b0ea0aaf86c9b38f2
stu1902 <stuart1902@gmail.com>
2022-06-04 16:50:19 +0100

    Weekly Update

--- services/db_connect.js
+++ services/db_connect.js
@@ -1,16 +1,18 @@
-const mysql = require('mysql');
+const mysql = require('mysql2');
 const dbConf = require ('./db_config.js');
-const conn = mysql.createConnection({
+const connection = mysql.createConnection({
     host: dbConf.db.host,
     user: dbConf.db.user,
     password: dbConf.db.password,
     database: dbConf.db.database,
+    waitForConnections: true,
+    connectionLimit: 10
 });
 
-conn.connect(error => {
+connection.connect(error => {
     if (error) throw error;
     console.log ("Connected to database ");
 });
 
 
-module.exports = conn;
+module.exports = connection;
