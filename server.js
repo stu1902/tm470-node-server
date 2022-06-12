afad8fca7aea4623bff21c5415b320bc0b7d93b5
stu1902 <stuart1902@gmail.com>
2022-06-12 11:42:36 +0100

    Weekly update

--- server.js
+++ server.js
@@ -6,15 +6,30 @@ const AppError = require("./utils/appError");
 const errorHandler = require("./utils/errorHandler");
 
 app.use('/', router);
+app.use(cors());
+app.use((req, res, next) => {
+    res.append('Access-Control-Allow-Origin', '*');
+    next();
+});
+
+/*
+app.get('/hello/:name?', (req, res) =>
+    res.json(
+        { message: `Hello ${req.params.name || 'world'}!` }
+    )
+);
+
+ */
+
 
 app.all("*", (req, res, next) => {
-  next(new AppError(`The URL ${req.originalUrl} does not exists`, 404));
+    next(new AppError(`The URL ${req.originalUrl} does not exists`, 404));
 });
 app.use(errorHandler);
 
 const PORT = 3000;
 app.listen(PORT, () => {
-  console.log(`server running on port ${PORT}`);
+    console.log(`server running on port ${PORT}`);
 });
 
 module.exports = app;
